5a42a758e321973d1df979649b7bb602016801b7
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2024-03-05 11:15:56 +0100

    demo

---
+++ jjopenai/jjopenai/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+using System.Net;
+using System.ComponentModel;
+
+// https://learn.microsoft.com/en-us/semantic-kernel/agents/
+
+string modelId = "jjconsole";
+string endpoint = "https://jjazscoai.openai.azure.com/";
+string apiKey = "4a9ecd0d36744b7999f0ffbb2e0ffdd7";
+
+// Create the kernel
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+builder.Plugins.AddFromType<AuthorEmailPlanner>();
+builder.Plugins.AddFromType<EmailPlugin>();
+Kernel kernel = builder.Build();
+
+// Retrieve the chat completion service from the kernel
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Create the chat history
+ChatHistory chatMessages = new ChatHistory("""
+You are a friendly assistant who likes to follow the rules. Your name is JJagent. You will complete required steps
+and request approval before taking any consequential actions. If the user doesn't provide
+enough information for you to complete a task, you will keep asking questions until you have
+enough information to complete the task.
+""");
+
+// Start the conversation
+while (true)
+{
+    // Get user input
+    System.Console.Write("User > ");
+    chatMessages.AddUserMessage(Console.ReadLine()!);
+
+    // Get the chat completions
+    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+    {
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+    };
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        chatMessages,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Stream the results
+    string fullMessage = "";
+    await foreach (var content in result)
+    {
+        if (content.Role.HasValue)
+        {
+            System.Console.Write("Assistant > ");
+        }
+        System.Console.Write(content.Content);
+        fullMessage += content.Content;
+    }
+    System.Console.WriteLine();
+
+    // Add the message from the agent to the chat history
+    chatMessages.AddAssistantMessage(fullMessage);
+}
---
+++ jjopenai/jjopenai/function.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+using System.Net;
+using System.ComponentModel;
+
+// https://learn.microsoft.com/en-us/semantic-kernel/prompts/templatizing-prompts?tabs=Csharp
+
+public class EmailPlugin
+{
+    [KernelFunction]
+    [Description("Sends an email to a recipient.")]
+    public async Task SendEmailAsync(
+        Kernel kernel,
+        [Description("Semicolon delimitated list of emails of the recipients")] string recipientEmails,
+        string subject,
+        string body
+    )
+    {
+        // Add logic to send an email using the recipientEmails, subject, and body
+        // For now, we'll just print out a success message to the console
+        Console.WriteLine("Email sent!");
+    }
+}
+
+public class AuthorEmailPlanner
+{
+    [KernelFunction]
+    [Description("Returns back the required steps necessary to author an email.")]
+    [return: Description("The list of steps needed to author an email")]
+    public async Task<string> GenerateRequiredStepsAsync(
+        Kernel kernel,
+        [Description("A 2-3 sentence description of what the email should be about")] string topic,
+        [Description("A description of the recipients")] string recipients
+    )
+    {
+        // Prompt the LLM to generate a list of steps to complete the task
+        var result = await kernel.InvokePromptAsync($"""
+        I'm going to write an email to {recipients} about {topic} on behalf of a user.
+        Before I do that, can you succinctly recommend the top 3 steps I should take in a numbered list?
+        I want to make sure I don't forget anything that would help my user's email sound more professional.
+        """, new() {
+            { "topic", topic },
+            { "recipients", recipients }
+        });
+
+        // Return the plan back to the agent
+        return result.ToString();
+    }
+}
